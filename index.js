225f918201445e7db939f432f979edb388a8b742
hassan-31x <muhammadhassanchannel786@gmail.com>
2024-12-30 23:53:08 +0500

    setup ejs

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 import express from 'express';
 import http from 'http'
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { Server } from 'socket.io';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 const server = http.createServer(app)
 const io = new Server(server, { cors: { origin: '*' } });
 
+//setup ejs files
+app.set("view engine", "ejs")
+app.use(express.static(path.join(__dirname, "public")))
+
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+})
+
 app.get('/', (req, res) => {
-  res.send('App Running');
+  res.render('index');
 })
 
 server.listen(4000, () => {
